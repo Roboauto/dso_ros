01c6cc5ec0083aa05cc3258aa7151957e82f73ab
Branislav Sevc <branislav.sevc@artin.cz>
2016-12-02 12:24:50 +0000

    changed camera topic name

--- src/main.cpp
+++ src/main.cpp
@@ -221,7 +221,7 @@ int main( int argc, char** argv )
     	fullSystem->setGammaFunction(undistorter->photometricUndist->getG());
 
     ros::NodeHandle nh;
-    ros::Subscriber imgSub = nh.subscribe("image", 1, &vidCb);
+    ros::Subscriber imgSub = nh.subscribe("/robo/camera/image_color", 1, &vidCb);
 
     ros::spin();
 
